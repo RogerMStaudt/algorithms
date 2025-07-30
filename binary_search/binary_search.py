0b7f7a16342d0dd2e98cd628bc6e4142c186c43c
Roger Miguel Staudt <rogermiguelstaudt@gmail.com>
2025-07-29 22:34:23 -0300

    Finishing binary search

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -1,21 +1,18 @@
 def search(list, guess):
     first = 0
     last = len(list) - 1
-    
 
     while first <= last:
-        middle = round(last / 2)
-
+        middle = round((first + last) / 2)
         if list[middle] > guess:
-            last = middle
+            last = middle - 1
         elif list[middle] < guess:
-            first = middle
+            first = middle + 1
         else:
             return middle
         
     return 'none'
 
-
 my_list = [1, 5, 10, 15, 20, 25, 30, 35, 40]
 
-print(search(my_list, 2))
+print(search(my_list, 25))
