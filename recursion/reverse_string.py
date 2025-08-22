e64a691611c31ca2cd0f0eee4201a8bd0f923121
Roger Miguel Staudt <rogermiguelstaudt@gmail.com>
2025-08-22 20:27:06 -0300

    Finish reverse_string

--- recursion/reverse_string.py
+++ recursion/reverse_string.py
@@ -1,7 +1,10 @@
-str = int(input("Write a string to reverse it: "))
+str = input("Write a string to reverse it: ")
 
-def reverse_string(string):
-    print(len(string) - 1)
+def reverse_string(string, reversed_str=''):
+    if string == '':
+        return reversed_str
+    
+    return reversed_str + reverse_string(string[0:len(string) - 1], string[-1])
 
 
 print(f'The reversed {str} string is {reverse_string(str)}')
