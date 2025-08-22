d2384a21e96ba6834aa256860d7ab6eb37472b60
Roger Miguel Staudt <rogermiguelstaudt@gmail.com>
2025-08-22 20:06:05 -0300

    Finish sum_list

--- recursion/sum_list.py
+++ recursion/sum_list.py
@@ -5,7 +5,31 @@ list = []
 while True:
     number = input("Write a number: ")
     
-    if number == 'quit':
+    if number == 'q':
         break
     
-    list.append(number)
+    list.append(int(number))
+
+def account():
+    account = ''
+
+    for i in list:
+        if account == '':
+            account = str(i)
+        else:
+            if i < 0:
+                account += f' + ({str(i)})'
+            else:
+                account += f' + {str(i)}'
+    
+    return account
+
+def sum_list(number):
+    if number == 0:
+        return list[number]
+
+    return list[number] + sum_list(number - 1)
+
+result = str(sum_list(len(list) - 1))
+
+print(account() + ' = ' + result)
