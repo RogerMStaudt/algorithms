50b23a41073ad0f1441049f888319681587cdbdb
Roger Miguel Staudt <rogermiguelstaudt@gmail.com>
2025-09-05 21:20:03 -0300

    Finish qs_users

--- quicksort/qs_users.py
+++ quicksort/qs_users.py
@@ -12,16 +12,15 @@ def quicksort(list):
     if len(list) < 2:
         return list
     else:
-        print(list[round((len(list) - 1) / 2)])
-        pivot = list[round((len(list) - 1) / 2)]['name']
+        pivot = list[round((len(list) - 1) / 2)]
         smaller_than_pivot = []
         greater_than_pivot = []
 
         for i in list:
-            if i['name'] < pivot:
-                smaller_than_pivot.append(i['name'])
-            elif i['name'] > pivot:
-                greater_than_pivot.append(i['name'])
+            if i['name'] < pivot['name']:
+                smaller_than_pivot.append(i)
+            elif i['name'] > pivot['name']:
+                greater_than_pivot.append(i)
 
         return quicksort(smaller_than_pivot) + [pivot] + quicksort(greater_than_pivot)
     
